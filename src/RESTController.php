19a8913ff5f47533f075b7b736d00092ab93c143
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2022-01-24 18:45:15 +0200

    validate version param in rest endpoint, delete secret after saving

--- src/RESTController.php
+++ src/RESTController.php
@@ -5,6 +5,7 @@ namespace Innocode\Prerender;
 use Innocode\Prerender\Traits\DbTrait;
 use WP_Error;
 use WP_Http;
+use WP_HTTP_Response;
 use WP_REST_Controller;
 use WP_REST_Request;
 use WP_REST_Response;
@@ -55,9 +56,10 @@ class RESTController extends WP_REST_Controller
                         'required'    => true,
                     ],
                     'version'         => [
-                        'description' => __( 'HTML version of the prerender.', 'innocode-prerender' ),
-                        'type'        => 'string',
-                        'required'    => true,
+                        'description'       => __( 'HTML version of the prerender.', 'innocode-prerender' ),
+                        'type'              => 'string',
+                        'required'          => true,
+                        'validate_callback' => [ $this, 'validate_version' ],
                     ],
                     'secret'          => [
                         'description' => __( 'Secret for the callback.', 'innocode-prerender' ),
@@ -115,6 +117,13 @@ class RESTController extends WP_REST_Controller
             return $converted_type_id;
         }
 
+        /**
+         * 'permission_callback' is also used after 'callback' in 'rest_send_allow_header' function through
+         * 'rest_post_dispatch' hook with priority 10, so, secret should be in place after 'callback' but still
+         * better to remove it after response returning as it cannot be used anymore after successful request.
+         */
+        add_filter( 'rest_post_dispatch', [ $this, 'delete_secret_hash' ], PHP_INT_MAX, 3 );
+
         list( $type, $object_id ) = $converted_type_id;
 
         $html = $request->get_param( 'html' );
@@ -138,5 +147,42 @@ class RESTController extends WP_REST_Controller
     public function url() : string
     {
         return rest_url( "/$this->namespace/$this->rest_base/" );
+    }
+
+    /**
+     * Removes secret before response returning.
+     *
+     * @param WP_HTTP_Response $result
+     * @param WP_REST_Server $server
+     * @param WP_REST_Request $request
+     *
+     * @return WP_HTTP_Response
+     */
+    public function delete_secret_hash(
+        WP_HTTP_Response $result,
+        WP_REST_Server $server,
+        WP_REST_Request $request
+    ) : WP_HTTP_Response
+    {
+        $type = $request->get_param( 'type' );
+        $id = $request->get_param( 'id' );
+
+        SecretsManager::delete( $type, $id );
+
+        return $result;
+    }
+
+    /**
+     * Checks if version is equal to current HTML version.
+     *
+     * @param string $param
+     *
+     * @return bool
+     */
+    public function validate_version( string $param ) : bool
+    {
+        $html_version = $this->get_db()->get_html_version();
+
+        return $html_version() == $param;
     }
 }
